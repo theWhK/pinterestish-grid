1eceab6b1a82ca2d96e8769ec0ce75728a841f53
Willian Hideki <willhkblz@gmail.com>
2015-03-29 19:25:08 -0300

    Changes in JavaScript
    Add support for flexible or responsive wrappers. If there are changes in
    the width of the wrapper, the element's positions will be recalculated.

--- jquery.pinterestish-grid.js
+++ jquery.pinterestish-grid.js
@@ -1,36 +1,41 @@
 $.fn.PinterestishGrid = function() {
-	/* Seletores de DOM reutilizáveis */
-	var wrapper = $(this);
-	var caixas = wrapper.children();
 
-	/* Cálculo de variáveis */
-	var num_max_caixas = Math.floor(wrapper.width() / caixas.outerWidth(true));
-	var margem_wrapper = (wrapper.width() - (num_max_caixas * caixas.outerWidth(true))) / 2;
-	var margem_caixas = ((wrapper.width() / num_max_caixas) - caixas.width()) / 2;
+	function calc_position() {
+		/* Seletores de DOM reutilizáveis */
+		var wrapper = $(this);
+		var caixas = wrapper.children();
 
-	/* Posicionamento das caixas */
-	caixas.each(function(index, el) {
-		if (index >= num_max_caixas) {
-			var divisor = Math.floor(index / num_max_caixas);
-			var index = index - (num_max_caixas * divisor);
-		}
+		/* Cálculo de variáveis */
+		var num_max_caixas = Math.floor(wrapper.width() / caixas.outerWidth(true));
+		var margem_wrapper = (wrapper.width() - (num_max_caixas * caixas.outerWidth(true))) / 2;
+		var margem_caixas = ((wrapper.width() / num_max_caixas) - caixas.width()) / 2;
 
-		var pos_top = 0;
-		$("[data-index='" + index + "']").each(function(index, el) {
-			pos_top += $(this).outerHeight(true);
+		/* Posicionamento das caixas */
+		caixas.each(function(index, el) {
+			if (index >= num_max_caixas) {
+				var divisor = Math.floor(index / num_max_caixas);
+				var index = index - (num_max_caixas * divisor);
+			}
+
+			var pos_top = 0;
+			$("[data-index='" + index + "']").each(function(index, el) {
+				pos_top += $(this).outerHeight(true);
+			});
+			var pos_left = ($(this).outerWidth(true) * index) + margem_wrapper;
+
+			$(this).attr('data-index', index);
+
+			$(this).css({
+				left: pos_left,
+				top: pos_top,
+				position: 'absolute',
+				float: 'none'
+			});
 		});
-		var pos_left = ($(this).outerWidth(true) * index) + margem_wrapper;
 
-		$(this).attr('data-index', index);
+		/* Estilização pós-reflow */
+		wrapper.css('position', 'relative');
+	}
 
-		$(this).css({
-			left: pos_left,
-			top: pos_top,
-			position: 'absolute',
-			float: 'none'
-		});
-	});
-
-	/* Estilização pós-reflow */
-	wrapper.css('position', 'relative');;
+	$(this).resize(calc_position());
 }
